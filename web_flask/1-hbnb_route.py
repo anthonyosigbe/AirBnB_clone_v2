7380711b479d4b10d1068195aa7e840ebf087df6
Anthony Osigbe <thronesofglory@gmail.com>
2024-01-25 05:39:18 -0800

    1. HBNB tasks completed

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
-"""Initiate a Flask-based web application."""
+"""Initiate a Flask-based web application.
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /: Shows 'Hello HBNB!'.
+    /hbnb: Displays 'HBNB'.
+"""
 
 from flask import Flask
 app = Flask(__name__)
