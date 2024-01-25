4917af75b89c1d40484cdaab7d5400b3bba0e71d
Anthony Osigbe <thronesofglory@gmail.com>
2024-01-25 14:03:29 -0800

    4. Is it a number? tasks completed

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""Launch the web application with two routes.
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    """Provide a string in response to a queried route.
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """When the route is queried, return a string.
+    """
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_is_fun(text):
+    """Return reformatted text
+    """
+    return 'C ' + text.replace('_', ' ')
+
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def python_with_text(text='is cool'):
+    """Reformat the text based on the presence
+    of an optional variable."""
+    return 'Python ' + text.replace('_', ' ')
+
+
+@app.route('/number/<int:n>')
+def number(n=None):
+    """Permit the request if the path variable is a valid integer.
+    """
+    return str(n) + ' is a number'
+
+if __name__ == '__main__':
+    app.url_map.strict_slashes = False
+    app.run(host='0.0.0.0', port=5000)
